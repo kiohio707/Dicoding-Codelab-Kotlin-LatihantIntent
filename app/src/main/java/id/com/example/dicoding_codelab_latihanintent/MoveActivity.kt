fee4e9ae555558cf1954b605bc54df3bf4df92a4
kiohio707 <fernata707@gmail.com>
2022-08-10 19:19:01 +0700

    Finish create explicit and implicit intent

---
+++ app/src/main/java/id/com/example/dicoding_codelab_latihanintent/MoveActivity.kt
@@ -0,0 +1,11 @@
+package id.com.example.dicoding_codelab_latihanintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
